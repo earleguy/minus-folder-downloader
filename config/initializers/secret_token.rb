b6f6c712e0ae281b44306e68c550336c7d69add3
adam <adam@cat.(none)>
2012-09-25 19:50:06 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anxair::Application.config.secret_token = '63d0d24067fcd2513319c27700c9041a7c060ca1ab9b4c97581aadb258ca3aac8867300e5170870f114ef6f492196545211c7bd17fa18834ab3e288ca2698b4b'
